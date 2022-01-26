a55e29e92b8bdb7c935d3bb07b4be5fd5aa8cc27
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-01-26 11:45:26 +0900

    一時パスワードの表示・時点

---
+++ src/main/kotlin/com/example/app/bbs/config/BbsAdminWebSecurityConfig.kt
@@ -0,0 +1,40 @@
+package com.example.app.bbs.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+
+@Configuration
+@EnableWebSecurity
+class BbsAdminWebSecurityConfig : WebSecurityConfigurerAdapter() {
+
+    @Autowired
+    lateinit var passwordEncoder: PasswordEncoder
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Override
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        val password: String =passwordEncoder.encode("root")
+
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password(password)
+//            .password(
+//                "\$2a\$10\$qHwwgwxqm5Od7dSdeIE4wu3lhLbA3xicU3IwPGQWsCxBu13vit7FK" // "$"に関してコンパイルエラーが出る場合は、直前に"\"を足してエスケープしても問題ない
+//            )
+            .authorities("ROLE_ADMIN")
+
+        println(password)
+//      logに出力された値： \$2a\$10$qHwwgwxqm5Od7dSdeIE4wu3lhLbA3xicU3IwPGQWsCxBu13vit7FK
+    }
+}
